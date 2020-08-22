8efa21416c42217d4fce0bd4879954c4473cb711
stigsanek <stigsanek@yandex.ru>
2020-08-22 21:34:22 +0400

    Добавляет модель

---
+++ src/model.js
@@ -0,0 +1,23 @@
+export const model = [
+  {
+    type: 'title', value: 'Конструктор сайтов на чистом JavaScript!', options: {
+      tag: 'h2',
+      styles: 'background: linear-gradient(to right, #ff0099, #493240);color: #fff;padding: 1.5rem;text-align: center;'
+    }
+  },
+  {
+    type: 'textColumns', value: [
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo dolorum quas tenetur. Deleniti omnis velit officia fugit ex nulla unde!',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo dolorum quas tenetur. Deleniti omnis velit officia fugit ex nulla unde!',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo dolorum quas tenetur. Deleniti omnis velit officia fugit ex nulla unde!'
+    ],
+    options: {
+      styles: 'padding: 2rem 0; color: #fff;background: linear-gradient(to bottom, #8e2de2, #4a00e0);font-weight: bold;'
+    }
+  },
+  {
+    type: 'text', value: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, ut ab! ', options: {
+      styles: 'background: linear-gradient(to left, #f2994a, #f2c94c);font-weight: bold;padding: 1rem;'
+    }
+  }
+];
